4c2388911f6351b3cbefac53560e14d19bc96e3f
Dam Huy Hung <h.damhuy@gmail.com>
2022-07-26 21:19:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import NewTodo from './Components/NewTodo';
+import TodoList from './Components/TodoList';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div  style={{marginTop: 40}}>
+      <h1>Todo list</h1>
+        <div>
+          <NewTodo />
+        </div>
+        <div>
+          <TodoList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoListItem.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import { deleteTodoAction } from "../redux/actions";
+
+export default function TodoListItem(props){
+  const dispatch = useDispatch()
+
+  const handleRemove = (id) =>{
+    dispatch(deleteTodoAction(id))
+};
+    return (
+        <div>
+        <h4>{ props.todo.text }</h4>
+        <button className="btn btn-danger" type="button" onClick={() => handleRemove(props.todo.id)}>
+          Remove
+        </button>
+        <hr/>
+      </div>
+    )
+}
